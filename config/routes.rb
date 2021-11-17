3215e0322d81736419feecb8be8dfe768a6ca3e3
kaiyukun <kaiyu.s0116@gmail.com>
2021-11-17 16:59:35 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
+  root 'posts#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
