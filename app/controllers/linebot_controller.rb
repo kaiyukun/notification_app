28856fb0e32772e51206808adfccb73391411cd4
kaiyukun <kaiyu.s0116@gmail.com>
2021-11-18 17:16:53 +0900

    もうちょい修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -45,24 +45,4 @@ class LinebotController < ApplicationController
         head :ok
     end
     
-    # def push
-    #     client = Line::Bot::Client.new { |config|
-    #         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    #         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    #     }
-    #     limit_tasks = Post.where(endday: Date.today)
-    #     limit_tasks.each do |user|
-    #         puts "名前：#{user.body}"
-    #         puts "ユーザーID：#{user.user_uid}"
-    #     end
-        
-    #     limit_tasks.each do |user|
-    #         message = {
-    #             type: 'text',
-    #             text: "「#{user.body}」の期限は今日でっせ！"
-    #         }
-    #         response = client.push_message(user.user_uid, message)
-    #         p response
-    #     end
-    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
   post '/callback' => 'linebot#callback'
-  post '/push' => 'linebot#push'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
